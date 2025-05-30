7cc4a00487bea94c7ab83642e146ef98495408ae
Dowes Mbella <dowesw@gmail.com>
2025-05-30 10:55:17 -0400

    Application des commentaires

--- src/main/java/com/lymytz/entitie/base/TypeDocEntity.java
+++ src/main/java/com/lymytz/entitie/base/TypeDocEntity.java
@@ -1,7 +1,16 @@
 package com.lymytz.entitie.base;
 
 import com.lymytz.entitie.BaseEntityWithSociete;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 

8c41e644a9cd060c4a8bae02fd7f7c206ecc66f4
Dowes Mbella <dowesw@gmail.com>
2025-05-30 10:19:43 -0400

    Ajout de l'entité UsersCodeAccesEntity.java

--- src/main/java/com/lymytz/entitie/base/TypeDocEntity.java
+++ src/main/java/com/lymytz/entitie/base/TypeDocEntity.java
@@ -1,16 +1,7 @@
 package com.lymytz.entitie.base;
 
 import com.lymytz.entitie.BaseEntityWithSociete;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 

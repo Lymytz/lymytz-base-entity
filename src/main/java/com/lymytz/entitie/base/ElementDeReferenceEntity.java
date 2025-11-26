6dbeb58b38497d22525f85015eb49d91adba1d32
gouchere <gouchereyves@gmail.com>
2025-11-26 07:31:37 +0100

    Add colum.

--- src/main/java/com/lymytz/entitie/base/ElementDeReferenceEntity.java
+++ src/main/java/com/lymytz/entitie/base/ElementDeReferenceEntity.java
@@ -4,12 +4,14 @@ import com.lymytz.entitie.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "yvs_base_element_reference")
 public class ElementDeReferenceEntity extends BaseEntity {
     @Id
     private Long id;
--- src/main/java/com/lymytz/entitie/base/SequenceNumerotationEntity.java
+++ src/main/java/com/lymytz/entitie/base/SequenceNumerotationEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.io.Serializable;
 @Entity
 @Getter
 @Setter
+@Table(name = "sequence_numerotation")
 @IdClass(SequenceNumerotationEntityPK.class)
 public class SequenceNumerotationEntity implements Serializable {
     @Column
